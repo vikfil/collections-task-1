5d71785352c391c7becae01709cb6a6612bbf856
relyativus <relyativus@gmail.com>
2018-01-21 19:52:25 +0200

    Created stub classes and test for task

---
+++ src/main/test/java/co/inventorsoft/academy/collections/model/RangeTest.java
@@ -0,0 +1,68 @@
+package co.inventorsoft.academy.collections.model;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(JUnit4.class)
+public class RangeTest {
+
+    @Test
+    public void sizeReturnsCountOfNumbersInInterval() {
+        Range<Integer> integerRange = Range.of(1, 10);
+        assertEquals(10, integerRange.size());
+    }
+
+    @Test
+    public void isEmptyWorksForBothEmptyAndNonEmptyIntervals() {
+        Range<Integer> integerRange = Range.of(1, 10);
+        assertFalse(integerRange.isEmpty());
+
+        Range<Integer> emptyRange = Range.of(1, 1);
+        assertTrue(emptyRange.isEmpty());
+    }
+
+    @Test
+    public void containsMatchesIfElementIsInTheRange() {
+        Range<Integer> integerRange = Range.of(1, 10);
+        assertTrue(integerRange.contains(5));
+    }
+
+    @Test
+    public void iteratorGeneratesIntegersIn1Step() {
+        Range<Integer> integerRange = Range.of(1, 10);
+        final List<Integer> expectedElements = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        assertEquals(10, integerRange.size());
+        for (Integer number : integerRange) {
+            assertTrue(expectedElements.contains(number));
+        }
+    }
+
+    @Test
+    public void iteratorGeneratesIntegersIn01Step() {
+        Range<Double> integerRange = Range.of(0.1, 0.5);
+        final List<Double> expectedElements = Arrays.asList(0.1, 0.2, 0.3, 0.4, 0.5);
+        assertEquals(5, integerRange.size());
+        for (Double number : integerRange) {
+            assertTrue(expectedElements.contains(number));
+        }
+    }
+
+    @Test
+    public void iteratorGeneratesCharactersForCustomType() {
+        final Range<Character> rangePoints = Range.of('a', 'd', prev -> (char) (prev + 1));
+        final Iterator<Character> iterator = rangePoints.iterator();
+        final List<Character> expectedCharacters = Arrays.asList('a', 'b', 'c', 'd');
+        for (Character character : expectedCharacters) {
+            assertEquals(0, iterator.next().compareTo(character));
+        }
+    }
+}
